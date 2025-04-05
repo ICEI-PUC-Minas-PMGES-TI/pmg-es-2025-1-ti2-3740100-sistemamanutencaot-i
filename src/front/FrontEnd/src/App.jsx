73709450e4b39562298cf5c2e70e5b8b6890e765
Gabriel Victor Souza Lopes <gabrieelvictor26@gmail.com>
2025-04-05 18:23:41 -0300

    Chamando tela todo na App.js

--- src/front/FrontEnd/src/App.jsx
+++ src/front/FrontEnd/src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import FormularioLoja from "./components/FormularioLoja.jsx";
+import CadastroLoja from "./components/CadastroLoja.jsx";
 
 function App() {
 
   return (
-
-  <FormularioLoja />
-
-  )
+    <>
+      <CadastroLoja />
+    </>
+  );
   
 }
 
