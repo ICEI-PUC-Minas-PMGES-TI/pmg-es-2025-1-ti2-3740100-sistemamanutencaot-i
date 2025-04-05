114340bc0a8240037709130b62c77789ee39a0c3
João Pedro Maciel de Oliveira <jpmaciel74@gmail.com>
2025-04-05 14:39:25 -0300

    Criação Componente Cadastro Loja

---
+++ src/front/FrontEnd/src/components/CadastroLojo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react';
+
+
+function CadastroLojo() {
+  const [age, setAge] = useState(28); 
+
+  return (
+    <>
+    <button>{}</button>
+    </>
+  )
+}
+
+
+
+export default CadastroLojo
