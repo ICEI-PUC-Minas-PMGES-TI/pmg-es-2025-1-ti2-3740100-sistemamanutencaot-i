64828cae4746f74b465756188537801fdbfba7c3
João Pedro Maciel de Oliveira <jpmaciel74@gmail.com>
2025-04-05 00:57:34 -0300

    Alteração projeto React

---
+++ src/front/FrontEnd/src/components/CadastroLojo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function CadastroLojo(props) {
+  return (
+    <div>CadastroLojo</div>
+  )
+}
+
+CadastroLojo.propTypes = {}
+
+export default CadastroLojo
