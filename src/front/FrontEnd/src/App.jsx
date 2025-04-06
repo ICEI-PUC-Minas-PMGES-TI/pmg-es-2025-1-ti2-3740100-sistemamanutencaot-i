94a1d4f0f5f12fe9ab86ae76b592550ea9fd756e
Gabriel Victor Souza Lopes <gabrieelvictor26@gmail.com>
2025-04-06 14:45:01 -0300

    Redirecionamento

--- src/front/FrontEnd/src/App.jsx
+++ src/front/FrontEnd/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import CadastroLoja from "./components/CadastroLoja.jsx";
+import CadastroLoja from "./pages/CadastroLoja.jsx";
 
 function App() {
 
