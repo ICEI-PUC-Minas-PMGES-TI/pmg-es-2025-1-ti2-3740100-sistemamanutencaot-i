dbe301a48106cfcd4a9dbfa739cab16331e6dbc8
Gabriel Victor Souza Lopes <gabrieelvictor26@gmail.com>
2025-04-06 21:17:25 -0300

    Add Redirecionamento de telas

--- src/front/FrontEnd/src/App.jsx
+++ src/front/FrontEnd/src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import CadastroLoja from "./pages/CadastroLoja.jsx";
 import CadastroLoginMaster from "./pages/CadastroLoginMaster.jsx";
 
 function App() {
-
   return (
-    <>
-      <CadastroLoginMaster />
-    </>
+    <Router>
+      <Routes>
+        {/* Rota para o cadastro da loja */}
+        <Route path="/cadastro-loja" element={<CadastroLoja />} />
+
+        {/* Rota para o cadastro do gerente */}
+        <Route path="/cadastro-gerente" element={<CadastroLoginMaster />} />
+
+        {/* Rota raiz (opcional - pode redirecionar) */}
+        <Route path="/" element={<CadastroLoja />} />
+      </Routes>
+    </Router>
   );
-  
 }
 
 export default App;
