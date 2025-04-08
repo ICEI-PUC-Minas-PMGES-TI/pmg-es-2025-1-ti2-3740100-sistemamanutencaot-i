4d43a55f386ffdf038f38c778c87a64f8ab003fc
Francisco Rafael P. Rodrigues <franciscocjn06@gmail.com>
2025-04-08 17:17:07 -0300

    Delete src/back/backend/src/main/java/io/manager/backend/GerenteRepository.java

--- src/back/backend/src/main/java/io/manager/backend/GerenteRepository.java
+++
@@ -1,7 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GerenteRepository extends JpaRepository<Gerente, Integer> {
-
-}
