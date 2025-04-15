e21e09887a35dbedb5221aa342464a19e8efedc1
Joao Ricardo Fiuza <joaoricfiuza@gmail.com>
2025-04-14 22:26:33 -0300

    Create TecnicoController.java

---
+++ src/back/backend/src/main/java/io/manager/backend/controller/TecnicoController.java
@@ -0,0 +1,53 @@
+package io.manager.backend.controller;
+
+import io.manager.backend.model.Tecnico;
+import io.manager.backend.service.TecnicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/tecnicos")
+@CrossOrigin (origins = {
+    "http://localhost:5173",
+    "https://pmg-es-2025-1-ti2-3740100-sistemamanutencaot-i.vercel.app"
+})
+public class TecnicoController {
+
+    @Autowired
+    private TecnicoService tecnicoService;
+
+    @GetMapping
+    public ResponseEntity<List<Tecnico>> listarTodos() {
+        return ResponseEntity.ok(tecnicoService.listarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tecnico> buscarPorId(@PathVariable Integer id) {
+        return ResponseEntity.ok(tecnicoService.buscarPorId(id));
+    }
+
+    @GetMapping("/email")
+    public ResponseEntity<Tecnico> buscarPorEmail(@RequestParam String email) {
+        return ResponseEntity.ok(tecnicoService.buscarPorEmail(email));
+    }
+
+    @PostMapping
+    public ResponseEntity<Tecnico> criar(@RequestBody Tecnico tecnico) {
+        Tecnico novo = tecnicoService.salvar(tecnico);
+        return ResponseEntity.ok(novo);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Tecnico> atualizar(@PathVariable Integer id, @RequestBody Tecnico tecnico) {
+        Tecnico atualizado = tecnicoService.atualizar(id, tecnico);
+        return ResponseEntity.ok(atualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Integer id) {
+        tecnicoService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
