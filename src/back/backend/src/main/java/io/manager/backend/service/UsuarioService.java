cb9663ea0a6e66c6c2b2999ccdd9df2d1f2e4e7b
João Ricardo Fiuza <156984266+JaoRicFiuza@users.noreply.github.com>
2025-05-09 13:42:35 -0300

    Create UsuarioService.java

---
+++ src/back/backend/src/main/java/io/manager/backend/service/UsuarioService.java
@@ -0,0 +1,33 @@
+package io.manager.backend.service;
+
+import io.manager.backend.model.Usuario;
+import io.manager.backend.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+    }
+
+    public List<Usuario> listarTodos() {
+        return usuarioRepository.findAll();
+    }
+
+    public Optional<Usuario> buscarPorLogin(String login) {
+        return usuarioRepository.findByLogin(login);
+    }
+}
