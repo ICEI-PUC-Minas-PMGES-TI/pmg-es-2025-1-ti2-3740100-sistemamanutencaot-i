f3ba8f2b14fa87868708c424230a085cefbe48e2
João Ricardo Fiuza <156984266+JaoRicFiuza@users.noreply.github.com>
2025-05-09 13:42:16 -0300

    Create PessoaJuridicaRepository.java

---
+++ src/back/backend/src/main/java/io/manager/backend/repository/PessoaJuridicaRepository.java
@@ -0,0 +1,10 @@
+package io.manager.backend.repository;
+
+import io.manager.backend.model.PessoaJuridica;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaJuridicaRepository extends JpaRepository<PessoaJuridica, Integer> {
+
+}
