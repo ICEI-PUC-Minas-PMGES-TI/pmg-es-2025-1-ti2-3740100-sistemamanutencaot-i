5b19623eac9c1feb758070e3ae0ef4967669da35
João Ricardo Fiuza <156984266+JaoRicFiuza@users.noreply.github.com>
2025-05-09 13:42:26 -0300

    Create PessoaFisicaService.java

---
+++ src/back/backend/src/main/java/io/manager/backend/service/PessoaFisicaService.java
@@ -0,0 +1,43 @@
+package io.manager.backend.service;
+
+import io.manager.backend.model.PessoaFisica;
+import io.manager.backend.repository.PessoaFisicaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PessoaFisicaService {
+
+    @Autowired
+    private PessoaFisicaRepository pessoaFisicaRepository;
+
+    public List<PessoaFisica> listarTodas() {
+        return pessoaFisicaRepository.findAll();
+    }
+
+    public Optional<PessoaFisica> buscarPorId(int id) {
+        return pessoaFisicaRepository.findById(id);
+    }
+
+    public PessoaFisica criar(PessoaFisica pessoaFisica) {
+        return pessoaFisicaRepository.save(pessoaFisica);
+    }
+
+    public Optional<PessoaFisica> atualizar(int id, PessoaFisica pessoaFisicaAtualizada) {
+        return pessoaFisicaRepository.findById(id).map(pessoaFisica -> {
+            pessoaFisica.setCpf(pessoaFisicaAtualizada.getCpf());
+            pessoaFisica.setPessoa(pessoaFisicaAtualizada.getPessoa());
+            return pessoaFisicaRepository.save(pessoaFisica);
+        });
+    }
+
+    public boolean deletar(int id) {
+        return pessoaFisicaRepository.findById(id).map(pessoaFisica -> {
+            pessoaFisicaRepository.delete(pessoaFisica);
+            return true;
+        }).orElse(false);
+    }
+}
