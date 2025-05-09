3b5caa85f587a75addf29b68789ce6d9caf86e80
João Ricardo Fiuza <156984266+JaoRicFiuza@users.noreply.github.com>
2025-05-09 13:41:36 -0300

    Create UsuarioController.java

---
+++ src/back/backend/src/main/java/io/manager/backend/controller/UsuarioController.java
@@ -0,0 +1,36 @@
+package io.manager.backend.controller;
+
+import io.manager.backend.model.Usuario;
+import io.manager.backend.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = {
+    "http://localhost:5173",
+    "https://pmg-es-2025-1-ti2-3740100-sistemamanutencaot-i.vercel.app"
+})
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario> criar(@RequestBody Usuario usuario) {
+        return ResponseEntity.ok(usuarioService.salvar(usuario));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(usuarioService.buscarPorId(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listarTodos() {
+        return ResponseEntity.ok(usuarioService.listarTodos());
+    }
+}
