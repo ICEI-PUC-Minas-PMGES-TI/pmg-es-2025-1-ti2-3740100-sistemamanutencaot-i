a1e1dd630b17f056c93497631e7b17bc3e949511
João Ricardo Fiuza <156984266+JaoRicFiuza@users.noreply.github.com>
2025-05-09 13:42:29 -0300

    Create PessoaJuridicaService.java

---
+++ src/back/backend/src/main/java/io/manager/backend/service/PessoaJuridicaService.java
@@ -0,0 +1,32 @@
+package io.manager.backend.service;
+
+import io.manager.backend.model.PessoaJuridica;
+import io.manager.backend.repository.PessoaJuridicaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PessoaJuridicaService {
+
+    @Autowired
+    private PessoaJuridicaRepository pessoaJuridicaRepository;
+
+    public List<PessoaJuridica> findAll() {
+        return pessoaJuridicaRepository.findAll();
+    }
+
+    public Optional<PessoaJuridica> findById(Integer id) {
+        return pessoaJuridicaRepository.findById(id);
+    }
+
+    public PessoaJuridica save(PessoaJuridica pessoaJuridica) {
+        return pessoaJuridicaRepository.save(pessoaJuridica);
+    }
+
+    public void deleteById(Integer id) {
+        pessoaJuridicaRepository.deleteById(id);
+    }
+}
