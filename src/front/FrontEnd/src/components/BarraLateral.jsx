786336ec2b781ee7a5933343a5a47ab0da9bce7e
Francisco Rafael P. Rodrigues <franciscocjn06@gmail.com>
2025-05-11 00:54:07 -0300

    Criando componente barra lateral

--- src/front/FrontEnd/src/components/BarraLateral.jsx
+++ src/front/FrontEnd/src/components/BarraLateral.jsx
@@ -1,17 +1,35 @@
 import React from "react";
 import styles from "../assets/css/BarraLateral.module.css"; // <- CSS como módulo
-import Logo from "../components/Logo.jsx";
+import Logo from "../assets/images/Logo simples.png";
 import Box from "../assets/images/Caixa.png"
+import User from "../assets/images/User.png"
+import Folder from "../assets/images/Folder plus.png"
+import Chave from "../assets/images/Chave.png"
+import Saida from "../assets/images/Log out.png"
 const BarraLateral = () => {
   return (
-    <div className={styles.containerPrincipal}>
+    <div className="mainContent">
+      <div className={styles.containerPrincipal}>
+        {/* Barra lateral */}
         <div className={styles.logo}>
-            <Logo />
+            <img src={Logo} alt="Logo" />
         </div>
 
         <div className={styles.opcoes}>
-            <img src={Box} alt="Imagem caixa" style={{ width: "100px", height: "100px" }} />
+            <img src={Box} alt="Caixa" />
+            <img src={User} alt="User" />
+            <img src={Folder} alt="Folder" />
+            <img src={Chave} alt="Chave" />
         </div>
+
+        <div className={styles.saida}>
+            <img src={Saida} alt="Saida" />
+        </div>
+      </div>
+
+      <div className="contentArea">
+        {/* Conteúdo principal aqui */}
+      </div>
     </div>
   );
 };
