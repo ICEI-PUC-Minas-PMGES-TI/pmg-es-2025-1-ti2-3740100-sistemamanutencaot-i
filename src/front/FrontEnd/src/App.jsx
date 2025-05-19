a7a1e41295d418672695c905ea63cff71cf15d68
GabrielVSL <gabrieelvictor26@gmail.com>
2025-05-19 08:30:40 -0300

    Ajustando cadastro de Tecnico

--- src/front/FrontEnd/src/App.jsx
+++ src/front/FrontEnd/src/App.jsx
@@ -2,13 +2,13 @@ import HomeTecnico from "./pages/HomeTecnico.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import CadastroLoja from "./pages/CadastroLoja.jsx";
-import CadastroLoginMaster from "./pages/CadastroLoginMaster.jsx";
+
 import CadastroTecnico from "./pages/CadastroTecnico.jsx";
 import TelaLogin from "./components/TelaLogin.jsx";
-import BarraLateral from "./components/BarraLateral.jsx";
-import ConfigGerente from "./pages/ConfiguracoesGerente.jsx"
-import Alert from "./components/AlterarSenhaAlert.jsx";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+import ConfigGerente from "./pages/ConfiguracoesGerente.jsx";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
@@ -28,9 +28,8 @@ function App() {
 
         <Route path="/configuracoes-gerente" element={<ConfigGerente />} />
 
-        <Route path="/alert" element={<Alert />} />
         {/* Rota raiz (opcional - pode redirecionar) */}
-        <Route path="/" element={<ConfigGerente />} />
+        <Route path="/" element={<CadastroTecnico />} />
       </Routes>
     </Router>
   );
--- src/front/FrontEnd/src/pages/CadastroTecnico.jsx
+++ src/front/FrontEnd/src/pages/CadastroTecnico.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import FormularioTecnico from "../components/FomularioTecnico.jsx";
 import BannerTecnico from "../components/BannerTecnico.jsx";
+import BarraLateral from "../components/BarraLateral.jsx";
 import styles from "../assets/css/CadastroTecnico.module.css";
 
 function CadastroTecnico() {
   return (
-    <div className={styles.containerHorizontal}>
-      <div className={styles.elementoEsquerdo}>
-        <BannerTecnico />
-      </div>
-      <div className={styles.elementoDireito}>
-        <FormularioTecnico />
+    <div className={styles.containerGeral}>
+      <BarraLateral />
+
+      <div className={styles.containerHorizontal}>
+        <div className={styles.elementoEsquerdo}>
+          <BannerTecnico />
+        </div>
+        <div className={styles.elementoDireito}>
+          <FormularioTecnico />
+        </div>
       </div>
     </div>
   );
