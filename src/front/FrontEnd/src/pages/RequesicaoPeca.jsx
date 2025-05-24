62882a712b2a6b8f5d719046f5ae19ba4a339d36
João Pedro Maciel <93168902+JpMaciel04@users.noreply.github.com>
2025-05-24 00:21:25 -0300

    Create RequesicaoPeca

---
+++ src/front/FrontEnd/src/pages/RequesicaoPeca.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BarraLateral from "../components/BarraLateral.jsx";
+
+
+function RequesicaoPeca() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default RequesicaoPeca
